633b8be4f8922072cdd0cc34f4a5d3298d886feb
Yusef Daramay <Yusef.Daramay@wnco.com>
2021-07-05 15:54:22 -0500

    video doesn't pause on android

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text } from "react-native"
+import React, { useState } from "react";
+import { View, Text, TouchableWithoutFeedback } from "react-native"
 import Video from 'react-native-video';
 import rap from '../../assets/images/rap.mp4'
 import outaPocket from '../../assets/images/outaPocket.mp4'
@@ -9,14 +9,24 @@ import grace from '../../assets/images/grace.mp4'
 
 import styles from "./styles"
 const Post = () => {
+
+    const [paused, setPause] = useState(false)
+    const onPlayPausePress = () => {
+        setPause(!paused)
+    }
     return (
         <View style={styles.container}>
-            <Video
-                source={grace}
-                style={styles.video}
-                resizeMode={'cover'}
-            />
-            <Text>Video not showing in ios</Text>
+            <TouchableWithoutFeedback onPress={onPlayPausePress}>
+                <Video
+                    source={grace}
+                    style={styles.video}
+                    resizeMode={'cover'}
+                    onError={(err) => console.log(err)}
+                    paused={paused}
+                // repeat={true}
+                />
+            </TouchableWithoutFeedback>
+            <Text>Tik Tok Clone</Text>
         </View>
     )
 };
